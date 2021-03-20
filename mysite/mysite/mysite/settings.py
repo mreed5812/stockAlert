2864a9f8b783537767b5c3e95e2f98f579162e37
Michael Reed <mreed5812@gmail.com>
2021-03-19 23:47:19 -0400

    created customerUser model.  Uses email, no username

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -29,15 +29,18 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTH_USER_MODEL = 'users.CustomUser'
 
 INSTALLED_APPS = [
-    'tracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'tracker',
+    'users',
 ]
 
 MIDDLEWARE = [
